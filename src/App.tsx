5bea62b405815f2e8a6da7a42f9096e5907b3552
NEPERSONAJ <nepersonaj@icloud.com>
2025-01-07 09:05:17 +0300

    Initial commit with all files

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { useState, useEffect } from 'react';
+import React, { useState, useEffect } from 'react';
 import { MessageCircle, ShoppingBag, Menu, Search } from 'lucide-react';
 import { cn } from './lib/utils';
 import { Button } from './components/ui/button';
